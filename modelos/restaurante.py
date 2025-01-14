44ae821b68267b8c484a686eb8cd1876bbc52531
murilo-fernando <murilo_oliveira199723@outlook.com>
2025-01-14 20:09:53 -0300

    Início da modelagem da classe Restaurante

---
+++ modelos/restaurante.py
@@ -0,0 +1,9 @@
+class Restaurante:
+    nome= ''
+    categoria= ''
+    ativo= ''
+
+restaurante_praca= Restaurante()
+restaurante_praca.nome= 'Praça'
+restaurante_praca.categoria= 'Gourmet'
+print(restaurante_praca.nome)
