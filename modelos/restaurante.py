5b452bac7783d795b584ad16120e9cb647d99696
murilo-fernando <murilo_oliveira199723@outlook.com>
2025-01-15 20:07:02 -0300

    Criação de construtores e definição de metodos especiais

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,9 +1,13 @@
 class Restaurante:
-    nome= ''
-    categoria= ''
-    ativo= ''
+    def __init__(self,nome,categoria):
+        self.nome= nome
+        self.categoria= categoria
+        self.ativo= False
 
-restaurante_praca= Restaurante()
-restaurante_praca.nome= 'Praça'
-restaurante_praca.categoria= 'Gourmet'
-print(restaurante_praca.nome)
+    def __str__(self):
+        return f'{self.nome} | {self.categoria}'
+
+restaurante_praca= Restaurante('Praça','Gourmet')
+restaurante_pizza= Restaurante('Pizza Express','Italiana')
+print(restaurante_praca)
+print(restaurante_pizza)
