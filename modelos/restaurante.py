f23085d05bc479f7ca207f8f6658b5f5705aea17
murilo-fernando <murilo_oliveira199723@outlook.com>
2025-01-20 20:01:46 -0300

    Modelagem de metodos e protegendo propriedades

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,13 +1,27 @@
 class Restaurante:
+    restaurantes =[]
+
     def __init__(self,nome,categoria):
-        self.nome= nome
-        self.categoria= categoria
-        self.ativo= False
+        self._nome= nome.title()
+        self._categoria= categoria.upper()
+        self._ativo= False
+        Restaurante.restaurantes.append(self)
 
     def __str__(self):
-        return f'{self.nome} | {self.categoria}'
+        return f'{self._nome} | {self._categoria}'
+
+
+    def listar_restaurantes():
+        print(f'{'NOME DO RESTAURANTE'.ljust(25)} | {'CATEGORIA'.ljust(25)} | {'STATUS'}')
+        for restaurante in Restaurante.restaurantes:
+            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante.ativo}')
+
+    @property
+    def ativo(self):
+        return '⌧' if self._ativo else '☐'
+
 
 restaurante_praca= Restaurante('Praça','Gourmet')
 restaurante_pizza= Restaurante('Pizza Express','Italiana')
-print(restaurante_praca)
-print(restaurante_pizza)
+
+Restaurante.listar_restaurantes()
